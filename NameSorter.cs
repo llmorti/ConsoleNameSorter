4b03c9e1ced8d1a1b520538b3a3f93ceadbe9b29
lloydEngineerm8 <lloyd@Engineerm8.com>
2025-09-17 00:21:29 +0100

    Add project files.

---
+++ NameSorter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace ConsoleNameSorter
+{
+    public static class NameSorter
+    {
+        public static List<string> SortNames(List<string> names)
+        {
+            var compareInfo = CultureInfo.InvariantCulture.CompareInfo;
+            // compare by last name then by given names considering non-ascii characters
+            // remove null entries
+            return names
+                .Where(name => !string.IsNullOrWhiteSpace(name))
+                .OrderBy(
+                    name => name.Split(new[] { ' ' }, System.StringSplitOptions.RemoveEmptyEntries).Last(),
+                    Comparer<string>.Create((x, y) => compareInfo.Compare(x, y, CompareOptions.None))
+                )
+                .ThenBy(
+                    name => string.Join(" ",
+                        name.Split(new[] { ' ' }, System.StringSplitOptions.RemoveEmptyEntries)
+                            .Take(name.Split(new[] { ' ' }, System.StringSplitOptions.RemoveEmptyEntries).Length - 1)
+                    ),
+                    Comparer<string>.Create((x, y) => compareInfo.Compare(x, y, CompareOptions.None))
+                )
+                .ToList();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace ConsoleNameSorter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Usage: name-sorter <file-path>");
+                return;
+            }
+
+            string inputFile = args[0];
+
+            if (!File.Exists(inputFile))
+            {
+                Console.WriteLine($"Error: File '{inputFile}' not found.");
+                return;
+            }
+
+            var names = File.ReadAllLines(inputFile)
+            .Where(line => !string.IsNullOrWhiteSpace(line))
+            .ToList();
+
+            var sortedNames = NameSorter.SortNames(names);
+
+            // Print to console
+            foreach (var name in sortedNames)
+            {
+                Console.WriteLine(name);
+            }
+
+            // Write to file
+            File.WriteAllLines("sorted-names-list.txt", sortedNames);
+        }
+    }
+}
